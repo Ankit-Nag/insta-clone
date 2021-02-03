7aa1bcc04ca18f1498cad8fcc22957c7e18e4d50
Ankit <ankitknag@gmail.com>
2021-02-03 22:24:43 +0530

    Update app.js
    learning to implement middlewares

--- server/app.js
+++ server/app.js
@@ -1,11 +1,22 @@
-const express = require('express')
-const app = express()
-const PORT = 5000
+const express = require('express');
+const app = express();
+const PORT = 5000;
 
-app.get('/home',(req,res)=>{
-    res.send("hello world!")
+const customMiddleware = (req, res, next) => {
+    console.log("middleware is executing!");
+    next();
+}
+
+app.get('/', (req, res) => {
+    console.log("home");
+    res.send("hello world!");
 })
 
-app.listen(PORT,()=>{
-    console.log("Server is running on ",PORT)
+app.get('/about',customMiddleware, (req, res) => {
+    console.log("about");
+    res.send("this is the about page!");
+})
+
+app.listen(PORT, () => {
+    console.log("Server is running on ", PORT);
 })
